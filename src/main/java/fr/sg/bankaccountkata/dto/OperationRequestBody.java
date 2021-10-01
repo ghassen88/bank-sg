960d52a5e9426a54a1b04a1a8ce2cec04a8c9093
ghassen88 <boussaaghassen2015@gmail.com>
2021-10-01 12:45:09 +0200

    first commit

---
+++ src/main/java/fr/sg/bankaccountkata/dto/OperationRequestBody.java
@@ -0,0 +1,13 @@
+package fr.sg.bankaccountkata.dto;
+
+public class OperationRequestBody {
+    private Double amount;
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+}
